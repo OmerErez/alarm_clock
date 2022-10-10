fe15d15367c98099ac9077b1339a44fa4d694322
Omer Erez <omer7135@gmail.com>
2022-10-10 16:28:40 +0300

    Use global db connection for create_db

--- create_db.py
+++ create_db.py
@@ -1,9 +1,8 @@
-from alarm_server.consts import DB_FILE_NAME
-from alarm_server.db_manager.db_manager import DBManager
+from alarm_server.consts import DB_MANAGER
 
 if __name__ == '__main__':
     if input("Are you sure? (enter Y): ") == 'Y':
-        with DBManager(DB_FILE_NAME) as db_manager:
+        with DB_MANAGER as db_manager:
             db_manager.execute_query("""
                 CREATE TABLE "Alarms" (
             "id"	INTEGER NOT NULL UNIQUE,
